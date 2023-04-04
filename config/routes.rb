6bcc9748c69812a94e6a40e502cca6b7c274cdcd
11as22 <ec2-user@ip-172-31-3-73.ap-northeast-1.compute.internal>
2023-04-04 09:28:41 +0000

    [Finish]3-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   get 'lists/new'
+  
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'top/' => 'homes#top'
   post 'lists' => 'lists#create'
   get 'lists' => 'lists#index'
-  get 'lists/edit'
- 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'lists/:id' => 'lists#show'
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
   get 'lists/:id' => 'lists#show', as: 'list'
+  patch 'lists/:id' =>'lists#update', as: 'update_list'
 end
 
